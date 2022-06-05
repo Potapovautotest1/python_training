b8084ef2c7ee39bf0c774e00d618c89f73236b87
Sergey <potapov.desigh@yandex.ru>
2022-06-05 14:26:50 +0300

    Передача параметров в методы, создание вспомогательного класса для хранения параметров группы

---
+++ Group.py
@@ -0,0 +1,6 @@
+
+class group:
+    def __init__(self, name_group, header, footer):
+        self.name_group=name_group
+        self.header=header
+        self.footer=footer
--- test_add_group.py
+++ test_add_group.py
@@ -3,19 +3,30 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time
+from Group import group
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group(wd)
-        self.init_create_new_group(wd)
+        self.init_create_new_group(wd, group(name_group="dfgkfgjk", header="fdkjgdl", footer="fdgjldgj"))
+        self.return_group_page(wd)
+        self.logout(wd)
+        time.sleep(5)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group(wd)
+        self.init_create_new_group(wd, group(name_group="", header="", footer=""))
         self.return_group_page(wd)
         self.logout(wd)
         time.sleep(5)
@@ -26,26 +37,26 @@ class TestAddGroup(unittest.TestCase):
     def return_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def init_create_new_group(self, wd):
+    def init_create_new_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("dfgkfgjk")
+        wd.find_element_by_name("group_name").send_keys(group.name_group)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("fdkjgdl")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("fdgjldgj")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def open_group(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
     def open_home_page(self, wd):
