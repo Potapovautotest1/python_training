a652f53da62578ff1c73e5faafcbf78cd0124411
Sergey <potapov.desigh@yandex.ru>
2022-06-10 21:26:21 +0300

    Создание класса помощника для теста создания контакта.

--- fixture/Appcontact.py
+++ fixture/Appcontact.py
@@ -1,79 +1,15 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
 
 class appcontact():
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.contact = ContactHelper(self)
 
-    def login(self, username="admin", password="secret"):
-        wd = self.wd
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def init_add_contact(self, contact):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phonenumber)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phonenumber)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.faxnumber)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("bday").send_keys(contact.bday)
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("bmonth").send_keys(contact.bmonth)
-        wd.find_element_by_xpath("//option[@value='July']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_name("aday").send_keys(contact.bday)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[10]").click()
-        wd.find_element_by_name("amonth").click()
-        wd.find_element_by_name("amonth").send_keys(contact.bmonth)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[8]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("new_group").click()
-        wd.find_element_by_name("new_group").send_keys(contact.group)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[5]/option[2]").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home").click()
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,12 +14,12 @@ def appc(request):
     
 def test_create_new_contact(appc):
     appc.open_home_page()
-    appc.login(username="admin", password="secret")
-    appc.init_add_contact(contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivaniv", nickname="Vanz",
+    appc.session.login(username="admin", password="secret")
+    appc.contact.init_add_contact(contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivaniv", nickname="Vanz",
                          title="GRS", company="GRS", address="Linina 12", home_phonenumber="7894568548", mobile_phonenumber="4854689797", work="GRS",
                          faxnumber="12345678", email="Faoy@email/com", bday="8", bmonth="July", byear="2000", ayear="2025",
                          group="dfgkfgjk", address2="linina 56", phone2="lenina 78", notes="fdjgjkfsdgiorewjtliokefjdl"))
-    appc.logout()
+    appc.session.logout()
 
 
 
